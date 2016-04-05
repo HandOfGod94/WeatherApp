078f0dfdf50728a0d0f9875ed979f2f1cc18fb37
HandOfGod94 <gahan94rakh@gmail.com>
2016-04-05 22:38:17 +0530

    Detail Activity added

--- app/src/main/java/com/example/gahan/weatherapp/ForecastFragment.java
+++ app/src/main/java/com/example/gahan/weatherapp/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.gahan.weatherapp;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -92,6 +94,18 @@ public class ForecastFragment extends Fragment
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(),DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT,forecast);
+                startActivity(intent);
+            }
+        });
+
         return rootView;
     }
 
