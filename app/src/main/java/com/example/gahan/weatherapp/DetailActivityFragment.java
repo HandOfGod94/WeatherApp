b7bae4cae9550dc3414c2d87a90c46d1bac86f73
HandOfGod94 <gahan94rakh@gmail.com>
2016-04-06 22:30:10 +0530

    Share intent added

--- app/src/main/java/com/example/gahan/weatherapp/DetailActivityFragment.java
+++ app/src/main/java/com/example/gahan/weatherapp/DetailActivityFragment.java
@@ -1,9 +1,14 @@
 package com.example.gahan.weatherapp;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,9 +18,27 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment
 {
+    public static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    public static final String FORECAST_STR_HASHTAG = "#WeatherApp";
+    public String mForecastStr;
 
     public DetailActivityFragment()
     {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.detailfragment, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        ShareActionProvider mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        mShareActionProvider.setShareIntent(createShareForecastIntent());
     }
 
     @Override
@@ -24,8 +47,18 @@ public class DetailActivityFragment extends Fragment
     {
         Intent intent = getActivity().getIntent();
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-        String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-        ((TextView) rootView.findViewById(R.id.textview_detail)).setText(forecastStr);
+        mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+        ((TextView) rootView.findViewById(R.id.textview_detail)).setText(mForecastStr);
         return rootView;
+    }
+
+    private Intent createShareForecastIntent()
+    {
+        Intent shareIntent= new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                mForecastStr+"-"+FORECAST_STR_HASHTAG);
+        return shareIntent;
     }
 }
--- app/src/main/java/com/example/gahan/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/gahan/weatherapp/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.gahan.weatherapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName() ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -52,12 +58,37 @@ public class MainActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings)
         {
-            Intent intent = new Intent(this,SettingsActivity.class);
+            Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
+        }
+
+        if (id==R.id.action_map)
+        {
+            openPreferredLocation();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
 
+    private void openPreferredLocation()
+    {
+        SharedPreferences prefs =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String location = prefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_defaultValue));
+
+        Uri geoLocation = Uri.parse("geo:0, 0?").buildUpon()
+                .appendQueryParameter("q", location).build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
+    }
 }
